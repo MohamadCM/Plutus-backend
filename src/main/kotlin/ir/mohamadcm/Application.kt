d67f2e6f33779d2718dd353ea96660f2da3b7903
mohamad <m.chamanmotlagh@gmail.com>
2023-10-04 15:23:32 +0200

    Fix imports

--- src/main/kotlin/ir/mohamadcm/Application.kt
+++ src/main/kotlin/ir/mohamadcm/Application.kt
@@ -2,13 +2,14 @@ package ir.mohamadcm
 
 import io.ktor.server.application.*
 import ir.mohamadcm.plugins.*
+import io.ktor.server.netty.*
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 fun Application.module() {
-//    configureSecurity()
-//    configureHTTP()
-//    configureSerialization()
-//    configureSockets()
+    configureSecurity()
+    configureHTTP()
+    configureSerialization()
+    configureSockets()
     configureRouting()
 }
